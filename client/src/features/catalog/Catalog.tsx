c2ca6aed96e4d5505e55b872d42acfa1f1c128cb
ErnestoHKirk <hooghkirk.e@gmail.com>
2024-01-28 17:04:24 -0600

    Add react-router w/ base routes

--- client/src/features/catalog/Catalog.tsx
+++ client/src/features/catalog/Catalog.tsx
@@ -1,19 +1,19 @@
-import { useState, useEffect } from "react";
-import { Product } from "../../app/models/product";
-import ProductList from "./ProductList";
+import { useState, useEffect } from 'react';
+import { Product } from '../../app/models/product';
+import ProductList from './ProductList';
 
 export default function Catalog() {
-  const [products, setProducts] = useState<Product[]>([]);
+    const [products, setProducts] = useState<Product[]>([]);
 
-  useEffect(() => {
-    fetch("http://localhost:5000/api/products")
-      .then((response) => response.json())
-      .then((data) => setProducts(data));
-  }, []);
-
-  return (
-    <>
-      <ProductList products={products} />
-    </>
-  );
+    useEffect(() => {
+      fetch('http://localhost:5000/api/products')
+        .then(response => response.json())
+        .then(data => setProducts(data));
+    }, [])
+  
+    return (
+        <>
+            <ProductList products={products} />
+        </>
+    )
 }
